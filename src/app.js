015181d4198f443b0fd8ac57756d236c8f28b471
dizzyplay <vovoboss@gmail.com>
2018-12-09 22:39:12 +0900

    commit for pull

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import './style.css'
 
+const axios = require('axios')
+
 
 window.$ = window.jQuery = $;
 
 AOS.init();
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "62ZrhZvwwJncOp6eqJmUgmAEUfFEqj6o4jh5nfPOBfvGWP2VKtDXIdKvtdzflETa";
+
+axios.get('http://localhost:8000/comment/1')
+    .then(function(res){
+      console.log(res)
+    })
+
 
 
